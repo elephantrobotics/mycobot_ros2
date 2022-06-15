d8845ee59101f2e46586f51b4a74425687be735f
weijian <weijian.wang@elephantrobotics.com>
2022-06-15 17:38:11 +0800

    add mecharm_pi ros2 urdf

--- mycobot_description/setup.py
+++ mycobot_description/setup.py
@@ -20,6 +20,7 @@ setup(
         ('share/' + package_name + '/urdf'+'/600_urdf', glob("urdf/600_urdf/*")),
         ('share/' + package_name + '/urdf'+'/mycobot', glob("urdf/mycobot/*")),
         ('share/' + package_name + '/urdf'+'/mycobot_pi', glob("urdf/mycobot_pi/*")),
+        ('share/' + package_name + '/urdf'+'/mecharm_pi', glob("urdf/mecharm_pi/*")),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
