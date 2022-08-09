4196e070e0511ee10b8bbc9336ae9f2613b6ca65
weijian <weijian.wang@elephantrobotics.com>
2022-08-09 11:45:51 +0800

    add 320pi ROS2

---
+++ mycobot_320/mycobot_320pi/mycobot_320pi/slider_control.py
@@ -0,0 +1,41 @@
+import rclpy
+# from pymycobot.mycobot import MyCobot
+from pymycobot.mycobotsocket import MyCobotSocket
+from rclpy.node import Node
+from sensor_msgs.msg import JointState
+
+
+class Slider_Subscriber(Node):
+    def __init__(self):
+        super().__init__("control_slider")
+        self.subscription = self.create_subscription(
+            JointState,
+            "joint_states",
+            self.listener_callback,
+            10
+        )
+        self.subscription
+
+        self.mc = MyCobotSocket("192.168.123.240", 9000)
+        self.mc.connect(serialport="/dev/ttyAMA0",baudrate="115200")
+    def listener_callback(self, msg):
+        print(msg.position)
+        data_list = []
+        for _, value in enumerate(msg.position):
+            data_list.append(value)
+
+        self.mc.send_radians(data_list, 80)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    slider_subscriber = Slider_Subscriber()
+    
+    rclpy.spin(slider_subscriber)
+    
+    slider_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
