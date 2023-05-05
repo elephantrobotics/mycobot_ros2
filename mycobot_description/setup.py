e39615ac7e37f9f87307106d28b578c2e8eeb2c3
wangWking <842749351@qq.com>
2023-05-05 11:47:31 +0800

    Add 260x3pi ros2 code, to be tested

--- mycobot_description/setup.py
+++ mycobot_description/setup.py
@@ -30,6 +30,7 @@ setup(
         
         ('share/' + package_name + '/urdf'+'/ultraArm_urdf', glob("urdf/ultraArm_urdf/*")),
         ('share/' + package_name + '/urdf'+'/mycobot_x3pi', glob("urdf/mycobot_x3pi/*")),
+        ('share/' + package_name + '/urdf'+'/260_x3pi', glob("urdf/260_x3pi/*")),
 
 
     ],
