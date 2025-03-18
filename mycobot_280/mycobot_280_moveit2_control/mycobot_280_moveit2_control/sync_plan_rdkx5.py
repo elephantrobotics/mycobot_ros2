821c174fbe46786728fa0fc6138c408190035eab
wangWking <842749351@qq.com>
2025-03-18 18:11:59 +0800

    opt 280 rdkx5 moveit2

--- mycobot_280/mycobot_280_moveit2_control/mycobot_280_moveit2_control/sync_plan_rdkx5.py
+++ mycobot_280/mycobot_280_moveit2_control/mycobot_280_moveit2_control/sync_plan_rdkx5.py
@@ -36,19 +36,33 @@ class Slider_Subscriber(Node):
         self.get_logger().info("port:%s, baud:%d" % (port, baud))
         self.mc = MyCobot280RDKX5(port, baud)
         time.sleep(0.05)
-        self.mc.set_fresh_mode(1)
-        time.sleep(0.05)
+        if self.mc.get_fresh_mode() == 0:
+            self.mc.set_fresh_mode(1)
+            time.sleep(0.05)
+
+        # RViz中目标顺序
+        self.rviz_order = [
+            'joint2_to_joint1',
+            'joint3_to_joint2',
+            'joint4_to_joint3',
+            'joint5_to_joint4',
+            'joint6_to_joint5',
+            'joint6output_to_joint6'
+        ]
 
     def listener_callback(self, msg):
-        print(msg.position)
+        # 创建字典将关节名称与位置值关联
+        joint_state_dict = {name: msg.position[i] for i, name in enumerate(msg.name)}
+        # 根据 RViz 顺序重新排列关节角度
         data_list = []
-        for _, value in enumerate(msg.position):
-            radians_to_angles = round(math.degrees(value), 2)
-            data_list.append(radians_to_angles)
-            
+        for joint in self.rviz_order:
+            # 获取弧度并转为角度
+            if joint in joint_state_dict:
+                radians_to_angles = round(math.degrees(joint_state_dict[joint]), 3)
+                data_list.append(radians_to_angles)
+ 
         print('data_list: {}'.format(data_list))
-
-        self.mc.send_angles(data_list, 50)
+        self.mc.send_angles(data_list, 80)
 
 
 def main(args=None):
