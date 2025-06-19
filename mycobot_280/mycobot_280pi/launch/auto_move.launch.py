ebe1a015798429675ec765d703f6f36c4dcdc732
BriceHuguesENS <brice.hugues@ens-rennes.fr>
2025-06-19 10:25:00 +0200

    indent

--- mycobot_280/mycobot_280pi/launch/auto_move.launch.py
+++ mycobot_280/mycobot_280pi/launch/auto_move.launch.py
@@ -50,7 +50,7 @@ def generate_launch_description():
     )
     res.append(robot_state_publisher_node)
 
-        follow_display_node = Node(
+    follow_display_node = Node(
         package="mycobot_280pi",
         executable="follow_display",
         name="follow_display",
