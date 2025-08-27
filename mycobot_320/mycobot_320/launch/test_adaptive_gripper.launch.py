ccb2ad71175cd0b7d76540f47900be0e0dbc296c
wangWking <842749351@qq.com>
2025-08-27 14:07:06 +0800

    perf(320M5 foxy):support dynamic serial port parameter input

--- mycobot_320/mycobot_320/launch/test_adaptive_gripper.launch.py
+++ mycobot_320/mycobot_320/launch/test_adaptive_gripper.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
         name="model",
         default_value=os.path.join(
             get_package_share_path("mycobot_description"),
-            "urdf/mycobot_320_m5_2022/mycobot_320_m5_2022_pro_adaptive_gripper.urdf"
+            "urdf/mycobot_320_m5_2022/mycobot_320_m5_2022_adaptive_gripper.urdf"
         )
     )
     res.append(model_launch_arg)
